11be5bfa4145e0d9191fce8745a253dd50ed40fa
svarzee <svarzee@gmail.com>
2019-12-11 17:06:37 +0100

    Add type checker

--- class_extensions/lib/src/class_extension_generator.dart
+++ class_extensions/lib/src/class_extension_generator.dart
@@ -2,7 +2,9 @@ import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
-abstract class ClassExtensionGenerator {
+abstract class ClassExtensionGenerator<T> {
+  TypeChecker get typeChecker => TypeChecker.fromRuntime(T);
+
   String generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep);
 }
