5f6a4af1d887c0ac0c7383403d4ab6eb3c2ac397
svarzee <svarzee@gmail.com>
2019-12-12 13:55:04 +0100

    Adapt extension generators and configure builder

--- class_extensions/lib/builder.dart
+++ class_extensions/lib/builder.dart
@@ -1,8 +1,23 @@
 import 'package:build/build.dart';
 import 'package:class_extensions/src/class_extension_generator.dart';
 import 'package:class_extensions/src/class_extensions_builder.dart';
+import 'package:class_extensions/src/dummy_builder.dart';
+import 'package:class_extensions/src/extension_end_generator.dart';
+import 'package:class_extensions/src/extension_start_generator.dart';
 
 final List<ClassExtensionGenerator> classExtensionGenerators = [];
 
 Builder classExtensionsBuilder(BuilderOptions options) =>
     ClassExtensionsBuilder(classExtensionGenerators, 'class_extensions');
+
+Builder extensionEndDummyBuilder(BuilderOptions options) =>
+    registerClassExtensionGenerator(ExtensionEndGenerator());
+
+Builder extensionStartDummyBuilder(BuilderOptions options) =>
+    registerClassExtensionGenerator(ExtensionStartGenerator());
+
+DummyBuilder registerClassExtensionGenerator(
+    ClassExtensionGenerator generator) {
+  classExtensionGenerators.add(generator);
+  return DummyBuilder();
+}
--- class_extensions/lib/src/extension_end_generator.dart
+++ class_extensions/lib/src/extension_end_generator.dart
@@ -1,9 +1,10 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
+import 'package:class_extensions/src/class_extension_generator.dart';
 import 'package:class_extensions_annotations/annotations.dart';
 import 'package:source_gen/source_gen.dart';
 
-class ExtensionEndGenerator extends GeneratorForAnnotation<Extension> {
+class ExtensionEndGenerator extends ClassExtensionGenerator<Extension> {
   @override
   String generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
